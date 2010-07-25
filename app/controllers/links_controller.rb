166cc77612a9a3c0bf05c61e0525dd6183459230
Stealth Phreak <stealthphreak@hotmail.com>
2010-07-25 12:26:10 +0530

    Added a generic acion to return links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,11 +1,34 @@
+require "rbscrape/linkscraper.rb"
+
 class LinksController < ApplicationController
+  
   def bing
+  
+  n = Bing.new "cat", 20
+  @links = n.getLinks
+	
+  
   end
 
   def google
+  @name = params[:name]
   end
 
   def yahoo
+  
+  end
+  
+  #Used only to get links for ajax purposes
+  def getter
+  
+  @classname = params[:clname]
+  @keyword = params[:key]
+  @nooflinks = params[:no].to_i
+  l = Kernel.const_get(@classname).new @keyword, @nooflinks
+  links =  l.getLinks
+  @links = links.flatten
+  return @links  
+  
   end
 
 end
